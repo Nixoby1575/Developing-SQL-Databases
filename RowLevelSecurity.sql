a8651d810c5002503b1ed852df2adf8c61d1710f
Alexis Jahir Franco Llivipuma <105693703+Nixoby1575@users.noreply.github.com>
2024-07-02 00:38:19 -0500

    Update RowLevelSecurity.sql

--- RowLevelSecurity.sql
+++ RowLevelSecurity.sql
@@ -14,29 +14,29 @@ ProcesadoPor Varchar(10)
 go
  -- ingreso de datos de ejemplo
 Insert into dbo.Ordenes 
-values(101,'Monitores','2016-08-11 00:34:51:090',100,'SOFIA')
+values(101,'Monitores','2016-11-08 00:34:51:090',100,'SOFIA')
 Insert into dbo.Ordenes 
 values(102,'Teclados CORP','2016-01-08 19:44:51:090',700,'SOFIA')
 Insert into dbo.Ordenes 
-values(103,'Memoria RAM','2015-08-19 19:44:51:090',1500,'SOFIA')
+values(103,'Memoria RAM','2015-19-08 19:44:51:090',1500,'SOFIA')
 Insert into dbo.Ordenes 
-values(102,'Disco Duro','2014-08-19 19:44:51:090',1099,'CLAUDIA')
+values(102,'Disco Duro','2014-19-08 19:44:51:090',1099,'CLAUDIA')
 Insert into dbo.Ordenes 
 values(101,'Web Cam','2014-08-04 19:44:51:090',5600,'CLAUDIA')
 Insert into dbo.Ordenes 
 values(103,'Ratones','2015-08-10 19:44:51:090',498,'HUGO')
 Insert into dbo.Ordenes 
-values(102,'Cable HDMI','2015-04-17 19:44:51:090',999,'HUGO')
+values(102,'Cable HDMI','2015-17-04 19:44:51:090',999,'HUGO')
 Insert into dbo.Ordenes 
-values(101,'Cable VGA','2015-08-21 19:44:51:090',543,'VICTOR')
+values(101,'Cable VGA','2015-21-08 19:44:51:090',543,'VICTOR')
 Insert into dbo.Ordenes 
 values(103,'Conectores RJ45','2015-08-06 19:44:51:090',876,'VICTOR')
 Insert into dbo.Ordenes 
-values(102,'Memory Stick','2015-08-26 19:44:51:090',665,'VICTOR')
+values(102,'Memory Stick','2015-26-08 19:44:51:090',665,'VICTOR')
 go
 --Creacion de la funcion de predicado que filtra que ordenes
 --fueron realizadas por cada usuario
--- Filtro ser· aplicada durante la ejecuciÛn de la consulta
+-- Filtro ser√° aplicada durante la ejecuci√≥n de la consulta
 
 Create Function dbo.fn_SeguridadOrdenes (@ProcesadoPor sysname)
 returns table with Schemabinding
@@ -47,7 +47,7 @@ dbo.Ordenes
 where @ProcesadoPor = user_name()  
 go
 --Creacion de la politica de seguridad al crearla
---aplicar· la limitaciÛn de acceso a los usuarios
+--aplicar√° la limitaci√≥n de acceso a los usuarios
 Create security Policy fn_seguridad
 add Filter Predicate
 dbo.fn_SeguridadOrdenes(ProcesadoPor)
